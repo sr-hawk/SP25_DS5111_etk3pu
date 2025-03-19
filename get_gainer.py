c46c2d16d779a8c184b53ec62bcd7224cbec0e49
sr-hawk <157434187+sr-hawk@users.noreply.github.com>
2025-03-19 14:48:19 -0400

    Initial call file for lab 6

---
+++ get_gainer.py
@@ -0,0 +1,38 @@
+import sys
+from bin.gainers.factory import GainerFactory
+from bin.gainers.base import ProcessGainer
+
+class ProcessGainer:
+    def __init__(self, gainer_downloader, gainer_normalizer):
+        self.downloader = gainer_downloader
+        self.normalizer = gainer_normalizer
+
+    def _download(self):
+        self.downloader.download()
+
+    def _normalize(self):
+        self.normalizer.normalize()
+
+    def _save_to_file(self):
+        self.normalizer.save_with_timestamp()
+
+    def process(self):
+        self._download()
+        self._normalize()
+        self._save_to_file()
+
+if __name__=="__main__":
+    # Our sample main file would look like this
+    import sys
+   
+    # Make our selection, 'one' choice
+    choice = sys.argv[1]
+
+    # let our factory get select the family of objects for processing
+    factory = GainerFactory(choice)
+    downloader = factory.get_downloader()
+    normalizer = factory.get_processor()
+
+    # create our process
+    runner = ProcessGainer(downloader, normalizer)
+    runner.process()
