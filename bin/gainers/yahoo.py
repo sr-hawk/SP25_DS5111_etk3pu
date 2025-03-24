14a8fff2b64d017fabd68f061e4e14c7a6c2e22c
sr-hawk <157434187+sr-hawk@users.noreply.github.com>
2025-03-23 23:11:34 -0400

    Update yahoo.py
    Pylint updates

--- bin/gainers/yahoo.py
+++ bin/gainers/yahoo.py
@@ -1,20 +1,21 @@
 import subprocess
-import pandas as pd
 import os
+import pandas as pd
 from .base import GainerDownload, GainerProcess
 
 class GainerDownloadYahoo(GainerDownload):
     def __init__(self):
         self.url = 'https://finance.yahoo.com/gainers'
         self.output_file = 'sample_data/ygainers.html'
-        
-        
+
     def download(self):
         print("Downloading yahoo gainers")
         os.makedirs(os.path.dirname(self.output_file), exist_ok=True) # Ensure directory exists
-        command = f"sudo google-chrome-stable --headless --disable-gpu --dump-dom --no-sandbox --timeout=5000 '{self.url}' > {self.output_file}"
+        command = 
+        f"sudo google-chrome-stable --headless --disable-gpu --dump-dom --no-sandbox --timeout=5000 '{self.url}' 
+        > {self.output_file}"
         subprocess.run(command, shell=True, check=True)
-        
+
 class GainerProcessYahoo(GainerProcess):
     def __init__(self):
         self.input_file = 'sample_data/ygainers.html'
