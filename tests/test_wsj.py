6776184a31f868fa365d25c585248d1c9a252021
sr-hawk <157434187+sr-hawk@users.noreply.github.com>
2025-03-23 23:41:40 -0400

    Update test_wsj.py
    updated process test

--- tests/test_wsj.py
+++ tests/test_wsj.py
@@ -9,15 +9,15 @@ def test_gainer_download_wsj():
     with patch("subprocess.run") as mock_subprocess:
         downloader.download()
         mock_subprocess.assert_called_once()
-
+        
 def test_gainer_process_wsj():
     processor = wsj.GainerProcessWSJ()
     assert processor.input_file == "sample_data/wjsgainers.html"
     assert processor.output_file == "sample_data/wjsgainers.csv"
 
-    with patch("pandas.read_html", return_value=[MagicMock()]) as mock_read_html:
+    mock_df = MagicMock(spec=pd.DataFrame) #Creates a magic mock that behaves like a dataframe.
+    with patch("pandas.read_html", return_value=[mock_df]) as mock_read_html:
         with patch("pandas.DataFrame.to_csv") as mock_to_csv:
             processor.normalize()
             mock_read_html.assert_called_once()
-            mock_to_csv.assert_called_once()
-            
+            mock_df.to_csv.assert_called_once() #calls the to_csv function from the mocked dataframe.
