f37aa956315236f4b066ed31ca33f329838342e7
sr-hawk <157434187+sr-hawk@users.noreply.github.com>
2025-03-23 23:48:31 -0400

    Update test_yahoo.py
    fixed test

--- tests/test_yahoo.py
+++ tests/test_yahoo.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock, patch
 from bin.gainers import yahoo
+import pandas as pd
 
 def test_gainer_download_yahoo():
     downloader = yahoo.GainerDownloadYahoo()
@@ -15,9 +16,9 @@ def test_gainer_process_yahoo():
     assert processor.input_file == "sample_data/ygainers.html"
     assert processor.output_file == "sample_data/ygainers.csv"
 
-    with patch("pandas.read_html", return_value=[MagicMock()]) as mock_read_html:
+    mock_df = MagicMock(spec=pd.DataFrame) #Creates a magic mock that behaves like a dataframe.
+    with patch("pandas.read_html", return_value=[mock_df]) as mock_read_html:
         with patch("pandas.DataFrame.to_csv") as mock_to_csv:
             processor.normalize()
             mock_read_html.assert_called_once()
-            mock_to_csv.assert_called_once()
-            
+            mock_df.to_csv.assert_called_once() #calls the to_csv function from the mocked dataframe.
