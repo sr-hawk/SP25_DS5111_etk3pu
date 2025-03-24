e65c41ca8ecb811021b957178c0e07f9ee4b717e
sr-hawk <157434187+sr-hawk@users.noreply.github.com>
2025-03-23 23:13:01 -0400

    Update test_environment.py
    Pylint update

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -9,4 +9,5 @@ def test_python_version():
     """Checks if the Python version is 3.10 or 3.11."""
     major, minor = sys.version_info[:2]
     expected_versions = [(3, 10), (3, 11)]
-    assert (major, minor) in expected_versions, f"Python version {major}.{minor} is not supported. Supported versions are 3.10 and 3.11."
+    assert (major, minor) in expected_versions, 
+    f"Python version {major}.{minor} is not supported. Supported versions are 3.10 and 3.11."
