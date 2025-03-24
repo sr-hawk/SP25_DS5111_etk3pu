ed6d2d274c24e6f2705a3176dc2afbfd0152f8ca
sr-hawk <157434187+sr-hawk@users.noreply.github.com>
2025-03-23 23:33:48 -0400

    Update test_get_gainer.py
    Removed unnecessary test

--- tests/test_get_gainer.py
+++ tests/test_get_gainer.py
@@ -20,13 +20,4 @@ def test_get_gainer_main():
 
         mock_factory.assert_called_once_with("yahoo")
         mock_process_gainer.assert_called_once_with(mock_downloader, mock_processor)
-        mock_process_gainer_instance.process.assert_called_once()
-
-def test_get_gainer_main_no_args():
-    with patch("sys.argv", ["get_gainer.py"]), \
-         patch("sys.exit") as mock_exit, \
-         patch("builtins.print") as mock_print:
-        get_gainer.main()
-        mock_print.assert_called_once_with("Usage: python get_gainer.py [yahoo|wsj]")
-        mock_exit.assert_called_once_with(1)
-             
+        mock_process_gainer_instance.process.assert_called_once()             
